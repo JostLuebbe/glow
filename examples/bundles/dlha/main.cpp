6c8ba9e7c0c77dc427d30d065b7b2a86f538fff7
Jost Luebbe <jostluebbe@utexas.edu>
2020-04-01 14:29:55 -0500

    updated

--- examples/bundles/dlha/main.cpp
+++ examples/bundles/dlha/main.cpp
@@ -295,8 +295,8 @@ static uint8_t *initMutableWeightVars(const BundleConfig &config) {
     size_t imageDataSizeInBytes = inputDims[0] * inputDims[1] * inputDims[2] * inputDims[3] * sizeof(float);
     printf("Copying image data into mutable weight vars: %lu bytes\n", imageDataSizeInBytes);
 
-    //  const SymbolTableEntry &inputGPUDataVar = getMutableWeightVar(config, "gpu_0_data");
-    //  memcpy(mutableWeightVarsAddr + inputGPUDataVar.offset, inputT, imageDataSizeInBytes);
+    const SymbolTableEntry &inputGPUDataVar = getMutableWeightVar(config, "input");
+    memcpy(mutableWeightVarsAddr + inputGPUDataVar.offset, inputT, imageDataSizeInBytes);
 
     return mutableWeightVarsAddr;
 }
