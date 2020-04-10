747191584f79473cff975c4dc4c510ce1857540b
Jost Luebbe <jostluebbe@utexas.edu>
2020-04-10 17:42:28 -0500

    updated

--- examples/bundles/dlha/main.cpp
+++ examples/bundles/dlha/main.cpp
@@ -320,80 +320,18 @@ static uint8_t *initMutableWeightVars(const BundleConfig &config) {
     loadImagesAndPreprocess(inputImageFilenames, inputT, inputDims);
 
     // Copy image data into the gpu_0/data input variable in the mutableWeightVars area.
-//    size_t imageDataSizeInBytes = inputDims[0] * inputDims[1] * inputDims[2] * inputDims[3] * sizeof(float);
-//    printf("Copying image data into mutable weight vars: %lu bytes\n", imageDataSizeInBytes);
+    size_t imageDataSizeInBytes = inputDims[0] * inputDims[1] * inputDims[2] * inputDims[3] * sizeof(float);
+    printf("Copying image data into mutable weight vars: %lu bytes\n", imageDataSizeInBytes);
 
-//    const SymbolTableEntry &inputGPUDataVar = getMutableWeightVar(config, "input");
-//    memcpy(mutableWeightVarsAddr + inputGPUDataVar.offset, inputT, imageDataSizeInBytes);
+    const SymbolTableEntry &inputGPUDataVar = getMutableWeightVar(config, "input");
+    memcpy(mutableWeightVarsAddr + inputGPUDataVar.offset, inputT, imageDataSizeInBytes);
 
     return mutableWeightVarsAddr;
 }
 
 static uint8_t *initActivations(const BundleConfig &config) { return static_cast<uint8_t *>(alignedAlloc(config, config.activationsMemSize)); }
 
-/*#define debug 1
-#ifdef debug
-void print_matrix(int rows, int cols, int *matrix) {
-    for (int i = 0; i < rows; i++) {
-//        printf("[");
-        for (int j = 0; j < cols; j++) {
-            if (j < cols - 1)
-                printf("%d ", matrix[i * rows + j]);
-            else
-                printf("%d", matrix[i * rows + j]);
-        }
-//        printf("]");
-        printf("\n");
-    }
-}
-#endif // debug*/
-
-void print_matrix(size_t rows, size_t cols, int ** a) {
-    printf("[");
-    for (size_t r = 0; r<rows; r++){
-        if (r == 0)
-            printf("[");
-        else
-            printf(" [");
-        for (size_t c = 0; c<cols; c++){
-            if (c < cols - 1)
-                printf("%d ",  a[r][c]);
-            else
-                printf("%d",  a[r][c]);
-        }
-        printf("]");
-        if (r < rows - 1)
-            printf("\n");
-    }
-    printf("]");
-    printf("\n");
-}
-
-
-int ** read_matrix(size_t rows, size_t cols, FILE *matrix_file){
-
-    int ** matrix = (int **) malloc(sizeof(int *) * rows);
-
-    for (int r = 0; r<rows; r++){
-        matrix[r] = (int *) malloc(sizeof(int) * cols);
-        for (int c = 0; c<cols; c++){
-            fscanf(matrix_file, "%d", &matrix[r][c]);
-        }
-    }
-
-    return matrix;
-}
-
 int main(int argc, char **argv) {
-/*    printf("argc: %d\n", argc);
-    printf("%s\n", argv[1]);
-
-    FILE *img_file;
-
-    img_file = fopen(argv[1], "r");
-
-    int** img = read_matrix(32,32,img_file);
-    print_matrix(32,32,img);*/
 
     parseCommandLineOptions(argc, argv);
     // Allocate and initialize constant and mutable weights.
