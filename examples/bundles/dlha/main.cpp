a4045221f1a16b172d1d96444e1260fdbbead041
Jost Luebbe <jostluebbe@utexas.edu>
2020-04-10 17:31:17 -0500

    updated

--- examples/bundles/dlha/main.cpp
+++ examples/bundles/dlha/main.cpp
@@ -159,9 +159,9 @@ bool readPngImage(const char *filename, std::pair<float, float> range, float *&i
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
             if (j < 32 - 1)
-                printf("%.1f ", imageT[i * 32 + j] * 255);
+                printf("%.1f ", imageT[i * 32 + j]);
             else
-                printf("%.1f", imageT[i * 32 + j] * 255);
+                printf("%.1f", imageT[i * 32 + j]);
         }
         printf("\n");
     }
