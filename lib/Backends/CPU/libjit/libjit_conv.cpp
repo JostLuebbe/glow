273987608d6314fb32ef2345269c582cd95289e3
Jost Luebbe <jostluebbe@utexas.edu>
2020-04-11 19:51:20 -0500

    updated

--- lib/Backends/CPU/libjit/libjit_conv.cpp
+++ lib/Backends/CPU/libjit/libjit_conv.cpp
@@ -477,7 +477,7 @@ void libjit_quantized_convolution_generic(ElemTy *outW, const ElemTy *inW, const
 
             for (int r = -(3 / 2); r <= (3 / 2); r++) {
                 for (int c = -(3 / 2); c <= (3 / 2); c++) {
-                    printf("Here\n");
+//                    printf("Here\n");
                     if (in_bounds(x + c, y + r, inWdims[2], inWdims[1])) {
 //                        sum += (inW[y + r][x + c] - inOffset) * (filterW[r + (kernel_h / 2)][c + (kernel_w / 2)] - filterOffset);
                         sum += (inW[y * inWdims[1] + x] - inOffset) * (filterW[(r + (3 / 2)) * kernel_w + (c + (3 / 2))] - filterOffset);
