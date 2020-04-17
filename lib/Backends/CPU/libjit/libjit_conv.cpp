25d1a175d1ec11372109c318333211108a30f8f1
Jost Luebbe <jostluebbe@utexas.edu>
2020-04-17 18:27:17 -0500

    glow conv call updated

--- lib/Backends/CPU/libjit/libjit_conv.cpp
+++ lib/Backends/CPU/libjit/libjit_conv.cpp
@@ -33,9 +33,9 @@
 //#include "example.h"
 
 extern void glow_conv(int8_t *result, const int8_t *inW, const int8_t *filterW,
-                      const int32_t *biasW, const uint32_t *outWdims,
-                      const uint32_t *inWdims, const uint32_t *filterWdims,
-                      const uint32_t *biasWdims, int32_t outOffset, int32_t inOffset,
+                      const int32_t *biasW, const dim_t *outWdims,
+                      const dim_t *inWdims, const dim_t *filterWdims,
+                      const dim_t *biasWdims, int32_t outOffset, int32_t inOffset,
                       int32_t filterOffset, int32_t biasOffset, int32_t biasPre,
                       int32_t biasPost, int32_t biasScale, int32_t outPre,
                       int32_t outPost, int32_t outScale);
